f0ca0ffbe5c766e9c123e3e82a217f27f34a7f9c
Shay Doron <shayd2110@gmail.com>
2020-08-29 16:00:15 +0300

    commit brfore join BackEnd code from Or

--- ui_theme/freelancer/js/cbpAnimatedHeader.js
+++ ui_theme/freelancer/js/cbpAnimatedHeader.js
@@ -1,36 +1,39 @@
+/* eslint-disable  */
 /**
  * cbpAnimatedHeader.js v1.0.0
  * http://www.codrops.com
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-var cbpAnimatedHeader = (function() {
-
+var cbpAnimatedHeader = (function () {
 	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-fixed-top' ),
+		header = document.querySelector(".navbar-fixed-top"),
 		didScroll = false,
 		changeHeaderOn = 300;
 
 	function init() {
-		window.addEventListener( 'scroll', function( event ) {
-			if( !didScroll ) {
-				didScroll = true;
-				setTimeout( scrollPage, 250 );
-			}
-		}, false );
+		window.addEventListener(
+			"scroll",
+			function (event) {
+				if (!didScroll) {
+					didScroll = true;
+					setTimeout(scrollPage, 250);
+				}
+			},
+			false
+		);
 	}
 
 	function scrollPage() {
 		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
+		if (sy >= changeHeaderOn) {
+			classie.add(header, "navbar-shrink");
+		} else {
+			classie.remove(header, "navbar-shrink");
 		}
 		didScroll = false;
 	}
@@ -40,5 +43,4 @@ var cbpAnimatedHeader = (function() {
 	}
 
 	init();
-
 })();
