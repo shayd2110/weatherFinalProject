255ab7b2ae0c7da48832a3086b6f6c822ee1a5e7
Shay Doron <shayd2110@gmail.com>
2020-09-05 16:09:17 +0300

    fix client1

--- client-copy/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import UsersList from "./UsersList";
-import Signup from "./Signup";
-import Login from "./Login";
-import UsersUpdate from "./UsersUpdate";
-import Home from "./Home";
-import Map from "./Map";
-
-export { UsersList, Signup, Login, UsersUpdate, Home, Map };
